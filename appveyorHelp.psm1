be2126b823899dedc88739274bec16e0dca0ee15
Patrick von Reth <vonreth@kde.org>
2015-07-24 15:06:59 +0200

    fix chain invoke

--- appveyorHelp.psm1
+++ appveyorHelp.psm1
@@ -48,7 +48,7 @@ function LogExecPrivate()
 function LogExec()
 {
     Write-Host $Args
-    LogExecPrivate $Args
+    $function:LogExecPrivate.Invoke($Args)
 }
 
 function CmakeImageInstall([string] $destDir)
