64a07e9e08d3d531b81d5b5a28f51df176fef04e
Patrick von Reth <vonreth@kde.org>
2015-07-30 12:37:13 +0200

    fix?

--- appveyorHelp.psm1
+++ appveyorHelp.psm1
@@ -119,7 +119,7 @@ function Install-CmakeGitModule([string] $url, [hashtable] $arguments)
     LogExec git clone -q --depth 1 $branch $url $module
     popd
     pushd  $env:APPVEYOR_BUILD_FOLDER\work\build\$module
-    LogExec cmake -G $script:CMAKE_GENERATOR $env:APPVEYOR_BUILD_FOLDER\work\git\$module -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT" ([string]$arguments["cmakeDefines"])
+    LogExec cmake -G $script:CMAKE_GENERATOR $env:APPVEYOR_BUILD_FOLDER\work\git\$module -DCMAKE_INSTALL_PREFIX="$CMAKE_INSTALL_ROOT" ($arguments["cmakeDefines"] -join " ")
     LogExec  $script:MAKE install
     popd
 }
