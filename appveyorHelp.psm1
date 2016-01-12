5d8d17130645d346d9320688233fd8546190f9b7
Hannah von Reth <vonreth@kde.org>
2016-01-12 15:59:38 +0100

    Fix syntax

--- appveyorHelp.psm1
+++ appveyorHelp.psm1
@@ -304,7 +304,7 @@ function NsisDeployImage([string] $scriptName)
     }else{
         $redist = "none"
     }
-    if($compiler.EndsWith("64")
+    if($compiler.EndsWith("64"))
     {
         $defaultinstdir="$PROGRAMFILES64"
     }else{
